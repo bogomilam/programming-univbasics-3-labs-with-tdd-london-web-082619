aa0aec9b66914e9451f9787710ffacebdbe64dad
bogomilmihaylov <bogomilmihaylov@live.com>
2019-08-10 22:33:49 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,7 @@
-do
   first_number = get_variable_from_file('./calculator.rb', "first_number")
  
-  expect(first_number).to be_an(Integer).or be_a(Float)
-end
+second_number =
+third_number =
+fourth_number = 
+fifth_number =
+sixth_number =
